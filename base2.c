00c49ee5bd22b9c4dc42562a12a88a755e82b123
Anthony8062 <kdh941224@naver.com>
2021-08-02 16:48:56 +0900

    added base2, 3 c file

---
+++ base2.c
@@ -0,0 +1,7 @@
+char data2[20] = "i am base2 c file";
+
+char* base2()
+{
+    char b;
+    return b;
+}
---
+++ base3.c
@@ -0,0 +1,7 @@
+char data3[20] = "i am base3 c file";
+
+char* base3()
+{
+    char b;
+    return b;
+}
